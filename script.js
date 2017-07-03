9e2e1e89445de35b0cdbe254f40f9d2a12b56d2b
Huascar <huascarmc@hotmail.com>
2017-07-03 20:09:37 +0100

    AJAX and IMG being blocked, attempt 1

--- script.js
+++ script.js
@@ -5,30 +5,26 @@ $(document).ready(function grid(boxes)
 //Create grid.
   var $row = $('<div class="row"></div>');
   var $col = $('<div class="col"></div>');
-  //Resize boxes assigning css.
       for (var i = 0; i < boxes; i++) {
           $row.append($col.clone());
       }
       for (var i = 0; i < boxes; i++) {
           $('#outerbox').append($row.clone());
-        $('.col').css({ 'height': +  Math.floor(400/boxes) - 2, 'width': + Math.floor(400/boxes) - 2});
+            //Resize boxes assigning css.
+             $('.col').css({ 'height': +  Math.floor(400/boxes) - 2, 'width': + Math.floor(400/boxes) - 2});
     }
     $('.col').on('hover', function() {
     var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     $(this).css('background-color', randomColorChange);
     });
-
-
 //Add functionality to navbar buttons.
-   $("#g").click(function() {
-    var boxes = prompt("Out of service");
-   });
+   /*$("#g").on('click', function grid(boxes);*/
 
    $("#r").on('click', function() {
       $('.row').children().css('background-color', 'white');
    });
 
-   $("#c").click(function() {
+   /*$("#c").click(function() {
      prompt("Out of service");
-   });
+   });*/
 });
