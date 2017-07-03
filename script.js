37b8493adeab14cce4ae380aa3b2182a63d73665
Huascar <huascarmc@hotmail.com>
2017-07-03 18:00:33 +0100

    Added functionality, change canvas size and colorful brush, success

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-$(document).ready(function(){
-   $('.col').mouseenter(function() {
-     $(this).addClass('colon');
-  });
+$(document).ready(function grid(boxes)
+{
+//Declare how many boxes in the grid.
+  var boxes = prompt("Choose gridsize");
+//Create grid.
+  var $row = $('<div class="row"></div>');
+  var $col = $('<div class="col"></div>');
+  //Resize boxes assigning css.
+      for (var i = 0; i < boxes; i++) {
+          $row.append($col.clone());
+      }
+      for (var i = 0; i < boxes; i++) {
+          $("#outerbox").append($row.clone());
+        $('.col').css({ "height": +  Math.floor(400/boxes) - 2, "width": + Math.floor(400/boxes) - 2});
+    }
+    $('.col').on('hover', function() {
+    var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    $(this).css('background-color', randomColorChange);
+    });
+
+
+//Add functionality to navbar buttons.
+   $("#g").click(function(gridsize) {
+    var boxes = prompt("Choose #x# for your canvas");
+   });
+
+   $("#r").click(function() {
+      $()
+   });
+
+   $("#c").click(function() {
+     prompt("Choose color: black, red or blue");
+   });
 });
