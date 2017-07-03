f3901792515fbe5bf084ad77037de84381179f6d
Huascar <huascarmc@hotmail.com>
2017-07-03 19:24:26 +0100

    Changed appearance

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function grid(boxes)
 {
 //Declare how many boxes in the grid.
-  var boxes = prompt("Choose gridsize");
+  var boxes = prompt('Choose gridsize');
 //Create grid.
   var $row = $('<div class="row"></div>');
   var $col = $('<div class="col"></div>');
@@ -10,8 +10,8 @@ $(document).ready(function grid(boxes)
           $row.append($col.clone());
       }
       for (var i = 0; i < boxes; i++) {
-          $("#outerbox").append($row.clone());
-        $('.col').css({ "height": +  Math.floor(400/boxes) - 2, "width": + Math.floor(400/boxes) - 2});
+          $('#outerbox').append($row.clone());
+        $('.col').css({ 'height': +  Math.floor(400/boxes) - 2, 'width': + Math.floor(400/boxes) - 2});
     }
     $('.col').on('hover', function() {
     var randomColorChange = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
@@ -20,15 +20,15 @@ $(document).ready(function grid(boxes)
 
 
 //Add functionality to navbar buttons.
-   $("#g").click(function(gridsize) {
-    var boxes = prompt("Choose #x# for your canvas");
+   $("#g").click(function() {
+    var boxes = prompt("Out of service");
    });
 
-   $("#r").click(function() {
-      $()
+   $("#r").on('click', function() {
+      $('.row').children().css('background-color', 'white');
    });
 
    $("#c").click(function() {
-     prompt("Choose color: black, red or blue");
+     prompt("Out of service");
    });
 });
